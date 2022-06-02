afe9c1f3855ab71c9f1594271e68d529ab403062
Jaredslocker21 <jaredslocker21@gmail.com>
2022-06-02 12:22:42 +0000

    fix bmi calc

--- assets/js/bmicalc.js
+++ assets/js/bmicalc.js
@@ -1,30 +1,31 @@
+
 let button = document.querySelector("#calcbtn");
-let result = document.querySelector("#height");
+let result = document.querySelector("#result");
 
-/**Accepting height and weight into f(x)
- */
+// Function for calculating BMI
 function calculateBMI() {
-    // Input height as a string
-    let height = document.querySelector("#height").value;
-    //Input weight as a string
-    let weight = document.querySelector("weight").value;
+  // take height and add it as a string
+  let height = document.querySelector("#height").value;
+  // take weight and add it as a string
+  let weight = document.querySelector("#weight").value;
 
 
-    // Calculation F(x)
-    if (height === "") || (weight === "") {
-        result.innerHTML = "Invalid Height"
+  // Correcting the values provided
+  if (height === "" || weight === "") {
+    result.innerHTML = "Invalid height"
+  } else {
+    
+    // Fixes up too two decimal points
+    const bmi = (weight / ((height * height)/ 10000)).toFixed(2);
+    // Results of BMI prints the result
+    if (bmi < 18.6) {
+      result.innerHTML = `Increase Calories: BMI is <span>${bmi}</span>`;
+    } else if (bmi >= 18.6 && bmi < 24.9) {
+      result.innerHTML = `Normal: BMI is <span>${bmi}</span>`;
     } else {
-
-
-        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-        //results of BMI plus message
-        if (bmi < 18.5) {
-            result.innerHTML = `Under Weight : <span>${bmi}</span>`;
-        } else if (bmi >= 18.6 && bmi < 24.9) {
-            result.innerHTML = `Average : <span>${bmi}</span>`;
-        } else {
-            result.innerHTML = `Overweight: <span>${bmi}</span>`
-        }
+      result.innerHTML = `Decrease Calories: BMI is <span>${bmi}</span>`;
     }
+  }
 }
-button.addEventListener("click", calculateBMI)
+
+button.addEventListener("click", calculateBMI);
