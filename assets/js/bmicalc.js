4e6165d2b0f92bfd6d460106d6f42549a9c37e53
Jaredslocker21 <jaredslocker21@gmail.com>
2022-06-02 15:53:36 +0000

    simplify bmi calc heightInCm/100

--- assets/js/bmicalc.js
+++ assets/js/bmicalc.js
@@ -1,28 +1,27 @@
 
-let button = document.querySelector("#calcbtn");
-let result = document.querySelector("#result");
+const button = document.querySelector("#calcbtn");
+const result = document.querySelector("#result");
 
 // Function for calculating BMI
 function calculateBMI() {
   // take value height and add it as a string
-  let height = document.querySelector("#height").value;
+  let heightInCm = document.querySelector("#height").value;
   // take value weight and add it as a string
   let weight = document.querySelector("#weight").value;
 
 
   // Correcting the values provided
-  if (height === "" || weight === "") {
+  if (heightInCm === "" || weight === "") {
     result.innerHTML = "Invalid Input"
   } else {
-    
-    // Fixes up too two decimal points
-    const bmi = (weight / ((height * height)/ 10000)).toFixed(2);
+    const heightInMtr = heightInCm/100;
+    const bmi = (weight / (heightInMtr * heightInMtr)).toFixed(2);
     // Results of BMI prints the result
     if (bmi < 18.5) {
       result.innerHTML = `Increase Calories: BMI is <span>${bmi}</span>`;
     } else if (bmi >= 18.6 && bmi < 24.9) {
       result.innerHTML = `Normal: BMI is <span>${bmi}</span>`;
-    } else (bmi < 25); {
+    } else {
       result.innerHTML = `Decrease Calories: BMI is <span>${bmi}</span>`;
     }
   }
