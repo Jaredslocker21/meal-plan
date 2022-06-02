880e2ac302519d0d26514f4f9e150b70a04c7daa
Jaredslocker21 <jaredslocker21@gmail.com>
2022-06-02 11:36:58 +0000

    add bmi js f(x) is broken

--- assets/js/bmicalc.js
+++ assets/js/bmicalc.js
@@ -1,1 +1,30 @@
-let button = document.querySelector(#calcbtn)
+let button = document.querySelector("#calcbtn");
+let result = document.querySelector("#height");
+
+/**Accepting height and weight into f(x)
+ */
+function calculateBMI() {
+    // Input height as a string
+    let height = document.querySelector("#height").value;
+    //Input weight as a string
+    let weight = document.querySelector("weight").value;
+
+
+    // Calculation F(x)
+    if (height === "") || (weight === "") {
+        result.innerHTML = "Invalid Height"
+    } else {
+
+
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        //results of BMI plus message
+        if (bmi < 18.5) {
+            result.innerHTML = `Under Weight : <span>${bmi}</span>`;
+        } else if (bmi >= 18.6 && bmi < 24.9) {
+            result.innerHTML = `Average : <span>${bmi}</span>`;
+        } else {
+            result.innerHTML = `Overweight: <span>${bmi}</span>`
+        }
+    }
+}
+button.addEventListener("click", calculateBMI)
