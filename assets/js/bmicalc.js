77b9da2fafd3ddbdc58e77184dab6331e02e5020
Jaredslocker21 <jaredslocker21@gmail.com>
2022-06-02 12:56:25 +0000

    fix bmi parameters <8.5 clarify, bmi <25,comments

--- assets/js/bmicalc.js
+++ assets/js/bmicalc.js
@@ -4,25 +4,25 @@ let result = document.querySelector("#result");
 
 // Function for calculating BMI
 function calculateBMI() {
-  // take height and add it as a string
+  // take value height and add it as a string
   let height = document.querySelector("#height").value;
-  // take weight and add it as a string
+  // take value weight and add it as a string
   let weight = document.querySelector("#weight").value;
 
 
   // Correcting the values provided
   if (height === "" || weight === "") {
-    result.innerHTML = "Invalid height"
+    result.innerHTML = "Invalid Input"
   } else {
     
     // Fixes up too two decimal points
     const bmi = (weight / ((height * height)/ 10000)).toFixed(2);
     // Results of BMI prints the result
-    if (bmi < 18.6) {
+    if (bmi < 18.5) {
       result.innerHTML = `Increase Calories: BMI is <span>${bmi}</span>`;
     } else if (bmi >= 18.6 && bmi < 24.9) {
       result.innerHTML = `Normal: BMI is <span>${bmi}</span>`;
-    } else {
+    } else (bmi < 25); {
       result.innerHTML = `Decrease Calories: BMI is <span>${bmi}</span>`;
     }
   }
